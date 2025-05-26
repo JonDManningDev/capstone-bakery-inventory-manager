6744a8e3b65a8aa9c4d9d1bc3613c8144bb983e7
Jonathan <jondmanning1986@gmail.com>
2025-05-25 20:07:34 -0400

    Add bcrypt to backend dependencies.

--- backend/src/employees/employees.service.js
+++ backend/src/employees/employees.service.js
@@ -5,26 +5,28 @@ const tableName = "employees";
 const SALT_ROUNDS = 12;
 
 async function create(newEmployee) {
-    const { firstName, lastName, email, password } = newEmployee;
+  const { firstName, lastName, email, password } = newEmployee;
 
-    const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
+  const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
 
-    const [newRecord] = await knex(tableName).insert({
-        first_name: firstName,
-        last_name: lastName,
-        email,
-        password_hash
-    }).returning(['first_name', 'last_name', 'email']);
+  const [newRecord] = await knex(tableName)
+    .insert({
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      password_hash,
+    })
+    .returning(["first_name", "last_name", "email"]);
 
-    return newRecord;
+  return newRecord;
 }
 
-// Account information is retrieve via 'email' because 'email' is set to unique in the database.
+// Account information is retrieve via 'email' because 'email' is set to UNIQUE in the database.
 function read(email) {
-    return knex(tableName).select('*').where({ email }).first();
+  return knex(tableName).select("*").where({ email }).first();
 }
 
 module.exports = {
-    create,
-    read,
+  create,
+  read,
 };
